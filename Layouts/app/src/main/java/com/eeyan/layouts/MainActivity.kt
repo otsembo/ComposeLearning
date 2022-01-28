e43882f2b49e41a066b368130e5e815224632b68
otsembo <okumu.otsembo@gmail.com>
2022-01-28 20:22:09 +0300

    LayoutsIntro

---
+++ Layouts/app/src/main/java/com/eeyan/layouts/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.eeyan.layouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.eeyan.layouts.ui.theme.LayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        // LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutsTheme {
+        PhotographerCard()
+    }
+}
